10c31431aeb4ef99300f8a5571fad57cbb0207c0
Caio Mendes <caiosamarone10@hotmail.com>
2022-01-27 20:53:58 -0300

    estrutura inicial do projeto

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Footer from './modules/Footer/Footer';
+import Header from './modules/Header/Header';
+import Login from './modules/Login/Login';
+import Home from './pages/Home/Home';
+
 function App() {
-  return <div>App react</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
